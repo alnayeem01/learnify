f3e13e1b70356336c378fb09b466c6886011979b
Al Nayeem <nayeem2019@hotmail.com>
2025-09-18 10:32:44 +0100

    updated get auto generated playlist

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -1,29 +1,13 @@
-import nodemailer from "nodemailer";
+
 import path from "path";
 import { MailtrapClient } from "mailtrap";
 
 import {
-  MAILTRAP_PASSWORD,
-  MAILTRAP_USER,
   SIGN_IN_URL,
-  VERIFICATION_EMAIL,
   MAILTRAP_API_KEY,
 } from "../utils/variables";
 import fs from "fs";
 import { generateTemplate } from "#/mail/template";
-
-const generateEmailTransporter = () => {
-  const transport = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: MAILTRAP_USER,
-      pass: MAILTRAP_PASSWORD,
-    },
-  });
-
-  return transport;
-};
 
 // Send Otp
 interface Profile {
@@ -42,7 +26,7 @@ export const sendVerificationMail = async (token: string, profile: Profile) => {
   });
 
   const sender = {
-    email: 'hello@alnayeem.com',
+    email: "hello@alnayeem.com",
     name: "Learnify App",
   };
   const recipients = [
@@ -97,35 +81,57 @@ interface Options {
 }
 export const sendForgetPasswordLink = async (options: Options) => {
   //genrate otp
-  const transport = generateEmailTransporter();
-
   const { email, link } = options;
 
   const message = `Hi, We just received a request that you forgot your password. No problem you can use this link below to set up a brand new password.`;
 
-  // mailtrap email
-  transport.sendMail({
-    to: email,
-    from: VERIFICATION_EMAIL,
+  const client = new MailtrapClient({
+    token: MAILTRAP_API_KEY,
+  });
+  const sender = {
+    email: "hello@alnayeem.com",
+    name: "Learnify App",
+  };
+  const recipients = [
+    {
+      email: email,
+    },
+  ];
+
+  const logoImage = fs.readFileSync(
+    path.join(__dirname, "../mail/images/logo.png")
+  );
+  const WelcomeImage = fs.readFileSync(
+    path.join(__dirname, "../mail/images/welcome.png")
+  );
+
+  client.send({
+    from: sender,
+    to: recipients,
     subject: "Reset Password Link",
     html: generateTemplate({
       title: "Forgot Password",
-      message,
-      link,
+      message: message,
+      link: link,
       logo: "cid:logo",
       banner: "cid:welcome",
       btnTitle: "Reset Password",
     }),
+    category: "Password reset",
     attachments: [
       {
         filename: "logo.png",
-        path: path.join(__dirname, "../mail/logo.png"), // to pass absolute path we used path module#
-        cid: "logo", //cid- Content id
+        content_id: "logo",
+        disposition: "inline",
+        content: logoImage,
+        type: "image/png",
       },
       {
         filename: "welcome.png",
-        path: path.join(__dirname, "../mail/welcome.png"), // to pass absolute path we used path module#
-        cid: "welcome", //cid- Content id
+        content_id: "welcome",
+        disposition: "inline",
+        content: WelcomeImage,
+        type: "image/png",
       },
     ],
   });
@@ -138,36 +144,60 @@ interface Item {
 }
 export const sendPasswordResetSuccesEmail = async (item: Item) => {
   //genrate otp
-  const transport = generateEmailTransporter();
 
   const { name, email } = item;
 
   const message = `${name}, we just updated your password. You can now sign in with new password.`;
 
-  // mailtrap email
-  transport.sendMail({
-    to: email,
-    from: VERIFICATION_EMAIL,
+  const client = new MailtrapClient({
+    token: MAILTRAP_API_KEY,
+  });
+  const sender = {
+    email: "hello@alnayeem.com",
+    name: "Learnify App",
+  };
+  const recipients = [
+    {
+      email: email,
+    },
+  ];
+
+  const logoImage = fs.readFileSync(
+    path.join(__dirname, "../mail/images/logo.png")
+  );
+  const WelcomeImage = fs.readFileSync(
+    path.join(__dirname, "../mail/images/welcome.png")
+  );
+
+   client.send({
+    from: sender,
+    to: recipients,
     subject: "Password Changed",
     html: generateTemplate({
       title: "Password changed successfully",
-      message,
+      message: message,
       link: SIGN_IN_URL,
       logo: "cid:logo",
       banner: "cid:welcome",
       btnTitle: "Log in",
     }),
+    category: "Passoword reser update!",
     attachments: [
       {
         filename: "logo.png",
-        path: path.join(__dirname, "../mail/logo.png"), // to pass absolute path we used path module#
-        cid: "logo", //cid- Content id
+        content_id: "logo",
+        disposition: "inline",
+        content: logoImage,
+        type: "image/png",
       },
       {
         filename: "welcome.png",
-        path: path.join(__dirname, "../mail/welcome.png"), // to pass absolute path we used path module#
-        cid: "welcome", //cid- Content id
+        content_id: "welcome",
+        disposition: "inline",
+        content: WelcomeImage,
+        type: "image/png",
       },
     ],
   });
+
 };
